76a2f4c70b284e88998798bba420b5539d7d6b6b
cki <simasiberia@yandex.ru>
2025-09-21 15:06:10 +0700

    feat: add foo for configure

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,25 @@
 #include <string>
 #include "simulation_params.hpp"
 
+void load_cfg(Common &params, Laser &laser){
+    try {
+        params = load_config<Common>("/app/cfg/common.json");
+        laser = load_config<Laser>("/app/cfg/laser_params.json");
+    } 
+    catch (const std::exception& e) {
+        std::cerr << "Ошибка конфигурации: " << e.what() << '\n';
+    }
+
+}
 
 int main() {
     std::cout << "Start" << std::endl;
     
     Common params;
-    try {
-        params = load_config<Common>("/app/cfg/common.json");
-    } 
-    catch (const std::exception& e) {
-        std::cerr << "Ошибка конфигурации: " << e.what() << '\n';
-    }
+    Laser laser;
+    std::cout << "Configuration..." << std::endl;
+    load_cfg(params, laser);
+    std::cout << "Configuration is done" << std::endl;
 
     std::cout << "End" << std::endl;
     return 0;
