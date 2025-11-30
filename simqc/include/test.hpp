8fb411d19e63161f8feab961d14e324b73954d2d
cki <simasiberia@yandex.ru>
2025-11-30 18:06:17 +0700

    fix(simqc): bb84

--- simqc/include/test.hpp
+++ simqc/include/test.hpp
@@ -1,5 +1,4 @@
 #pragma once
-
 #include "sequence_generator.hpp"
 #include "laser.hpp"
 #include "modulator.hpp"
@@ -7,13 +6,8 @@
 #include "beam_splitter.hpp"
 #include "photodetector.hpp"
 
-
 class TestBB84{
 private:
-    // ILaser& laser;
-    // IModulator& modulator;
-    // IQuantumChannel& q_channel;
-    // SequenceGenerator seq_gen;
     std::vector<Pulse> send_pulse(ILaser& laser, IModulator& modulator, Common& params, LaserData& laser_data);
 public:
     void run(Common& params, LaserData& laser_data, QuantumChannelData& q_channel_data, PhotodetectorData& ph_data);
